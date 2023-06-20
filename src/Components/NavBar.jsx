dec74cd217d053150c5447a95f0f379cdc363235
Brodaty95 <jangargas95@gmail.com>
2023-06-20 13:53:43 +0200

    Pierwszy sukces

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="Main">
+      <h1>Strona przyszłego ojca</h1>
+      <nav>
+        <button>Strona główna</button>
+        <button>Przydatne funkcje</button>
+        <button>Ciekawe funkcje</button>
+        <button>O mnie</button>
+      </nav>
+    </div>
+  );
+}
+export default Main;
